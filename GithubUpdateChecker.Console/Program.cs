ceca94e5d2a78fc801836ec2fb9308f48f3d7fd5
Thomas <thomaspmar@protonmail.com>
2021-04-02 15:43:50 -0700

    Add project files.

---
+++ GithubUpdateChecker.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GithubUpdateChecker.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Github Update Checker!");
+
+            string url = "https://api.github.com/repos/HerpDerpinstine/MelonLoader/releases";
+            var updater = new UpdateChecker(url);
+            var releaseInfo = await updater.GetLatestReleaseAsync();
+            Console.WriteLine($"Got release info from Gtihub");
+
+            const string currentVersion = "0.2.9";
+            Console.WriteLine($"Current Version is: {currentVersion}");
+            Console.WriteLine($"Latest Version is: {releaseInfo.TagName}");
+
+            bool isUpdate = updater.IsUpdate(currentVersion, releaseInfo);
+            Console.WriteLine($"Is an update avalible: {isUpdate}");
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GithubUpdateCheckter.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GithubUpdateCheckter.Console
+{
+    using System;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //string url = "https://api.github.com/repos/gurrenm3/BloonsTD6-Mod-Helper/releases";
+            //string url = "https://api.github.com/repos/TDToolbox/BTD6-Mod-Manager/releases";
+            string url = "https://api.github.com/repos/HerpDerpinstine/MelonLoader/releases";
+            var updater = new GithubUpdateChecker.Updater(url);
+            var releaseInfo = await updater.GetReleaseInfoAsync();
+
+            Console.WriteLine("Got release info");
+
+            Console.WriteLine(releaseInfo.Count);
+
+            Console.WriteLine("Done");
+        }
+    }
+}
